806cdf6137ef7254393895d27baa59f019b78657
wongsitu <wongsitu@ksu.edu>
2018-12-09 21:46:15 -0800

    Markers added in map

--- couponBank/static/app/map.js
+++ couponBank/static/app/map.js
@@ -1,27 +1,38 @@
 $(document).ready(function() {
-console.log("Let's get coding!");
 
-function displayMap(){
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            var pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            }
-            var mapHere = new google.maps.Map(document.getElementById('map'), {
-                center: pos,
-                zoom: 12.5,
-            });
-            var marker = new google.maps.Marker({
-                position: pos,
-                map: mapHere,
-                title: 'Hello World!'
-            });
-            return mapHere;
-        })
-    };
-}
+    function displayMap(){
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                var pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                }
+                var mapHere = new google.maps.Map(document.getElementById('map'), {
+                    center: pos,
+                    zoom: 12.5,
+                });
 
-displayMap();
+                var marker = new google.maps.Marker({
+                    position: pos,
+                    map: mapHere,
+                });
 
+                $('#options').on('click','.coordinates',function(e){
+                    e.preventDefault
+                    lat = parseFloat($(this).attr('lat'))
+                    lng = parseFloat($(this).attr('lng'))
+                    rest_pos = {lat,lng}
+                    console.log(rest_pos)
+                    var marker = new google.maps.Marker({
+                        position: rest_pos,
+                        map: mapHere,
+                    });
+                })
+
+                return mapHere;
+            })
+        };
+    }
+
+    displayMap()
 });
